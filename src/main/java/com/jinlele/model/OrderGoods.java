cfec4398a2b3636cd7f85dfabccc66a997aa53a9
40679816@qq.com <a18863302302>
2016-12-20 18:22:42 +0800

    新增订单相关类

---
+++ src/main/java/com/jinlele/model/OrderGoods.java
@@ -0,0 +1,75 @@
+package com.jinlele.model;
+
+import java.util.Date;
+
+public class OrderGoods {
+    private Integer id;
+
+    private String shoporderNo;
+
+    private Integer goodId;
+
+    private String comment;
+
+    private String deletecode;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getShoporderNo() {
+        return shoporderNo;
+    }
+
+    public void setShoporderNo(String shoporderNo) {
+        this.shoporderNo = shoporderNo == null ? null : shoporderNo.trim();
+    }
+
+    public Integer getGoodId() {
+        return goodId;
+    }
+
+    public void setGoodId(Integer goodId) {
+        this.goodId = goodId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment == null ? null : comment.trim();
+    }
+
+    public String getDeletecode() {
+        return deletecode;
+    }
+
+    public void setDeletecode(String deletecode) {
+        this.deletecode = deletecode == null ? null : deletecode.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
