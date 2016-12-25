38b7c9f8b2dc7e6d9d4aea425c8aeaa42a6afe9e
twislyn <hampson163@163.com>
2016-12-25 11:34:28 +0800

    更新user类，新增地址类

---
+++ src/main/java/com/jinlele/dao/ReceiptAddressMapper.java
@@ -0,0 +1,17 @@
+package com.jinlele.dao;
+
+import com.jinlele.model.ReceiptAddress;
+
+public interface ReceiptAddressMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ReceiptAddress record);
+
+    int insertSelective(ReceiptAddress record);
+
+    ReceiptAddress selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ReceiptAddress record);
+
+    int updateByPrimaryKey(ReceiptAddress record);
+}
---
+++ src/main/java/com/jinlele/model/ReceiptAddress.java
@@ -0,0 +1,105 @@
+package com.jinlele.model;
+
+import java.util.Date;
+
+public class ReceiptAddress {
+    private Integer id;
+
+    private String linkman;
+
+    private String linkphone;
+
+    private String area;
+
+    private String detail;
+
+    private String post;
+
+    private String memo;
+
+    private String deletecode;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLinkman() {
+        return linkman;
+    }
+
+    public void setLinkman(String linkman) {
+        this.linkman = linkman == null ? null : linkman.trim();
+    }
+
+    public String getLinkphone() {
+        return linkphone;
+    }
+
+    public void setLinkphone(String linkphone) {
+        this.linkphone = linkphone == null ? null : linkphone.trim();
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area == null ? null : area.trim();
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail == null ? null : detail.trim();
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post == null ? null : post.trim();
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo == null ? null : memo.trim();
+    }
+
+    public String getDeletecode() {
+        return deletecode;
+    }
+
+    public void setDeletecode(String deletecode) {
+        this.deletecode = deletecode == null ? null : deletecode.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
