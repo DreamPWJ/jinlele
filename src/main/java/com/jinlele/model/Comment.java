a412b1d1ea2867a8df600313838d3c83e18a1317
690328661@qq.com <690328661@qq.com>
2016-12-27 08:42:47 +0800

    评论实体

---
+++ src/main/java/com/jinlele/model/Comment.java
@@ -0,0 +1,75 @@
+package com.jinlele.model;
+
+import java.util.Date;
+
+public class Comment {
+    private Integer id;
+
+    private Integer userId;
+
+    private String url;
+
+    private String deletecode;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    public String getDeletecode() {
+        return deletecode;
+    }
+
+    public void setDeletecode(String deletecode) {
+        this.deletecode = deletecode == null ? null : deletecode.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+}
