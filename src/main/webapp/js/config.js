59decf529c67f35a4b79e9ebd8043574468ad067
406798106@qq.com <406798106@qq.com>
2016-12-28 09:59:18 +0800

    配置文件域名修改

--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -7,11 +7,11 @@ var configMod = angular.module("starter.config", []);
 configMod.constant("JinLeLe", {
     "name": "JinLeLe", //项目名称
     "debug": false, //调试标示 暂无使用
-    "api": "http://127.0.0.1:8080/jinlele",//接口服务地址  使用
-   // "api": "http://58.58.180.104:8081/jinlele",//接口服务地址  使用
+    "api": "http://www.6weiyi.com/jinlele",//接口服务地址  使用
     'siteUrl': "http://a.jinlele.com",//仓库地址 暂无使用
     'imgUrl': "http://f.jinlele.com",//图片地址 暂无使用
     'moblileApi': "http://m.jinlele.com",//手机端服务
     'version': '0.1.0' //当前版本号
 });
+
 
