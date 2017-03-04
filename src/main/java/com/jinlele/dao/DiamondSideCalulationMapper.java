6edbd7ad109991232058689b4cc7ba0b864a34fa
690328661@qq.com <690328661@qq.com>
2017-03-04 14:48:14 +0800

    副石实体类

---
+++ src/main/java/com/jinlele/dao/DiamondSideCalulationMapper.java
@@ -0,0 +1,17 @@
+package com.jinlele.dao;
+
+import com.jinlele.model.DiamondSideCalulation;
+
+public interface DiamondSideCalulationMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(DiamondSideCalulation record);
+
+    int insertSelective(DiamondSideCalulation record);
+
+    DiamondSideCalulation selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(DiamondSideCalulation record);
+
+    int updateByPrimaryKey(DiamondSideCalulation record);
+}
---
+++ src/main/java/com/jinlele/model/DiamondSideCalulation.java
@@ -0,0 +1,65 @@
+package com.jinlele.model;
+
+import java.util.Date;
+
+public class DiamondSideCalulation {
+    private Integer id;
+
+    private Double price;
+
+    private String quality;
+
+    private String deletecode;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality == null ? null : quality.trim();
+    }
+
+    public String getDeletecode() {
+        return deletecode;
+    }
+
+    public void setDeletecode(String deletecode) {
+        this.deletecode = deletecode == null ? null : deletecode.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
