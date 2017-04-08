ca4d0890aa9988b46aad1a22850f3256966e59ae
690328661@qq.com <690328661@qq.com>
2017-04-08 13:47:44 +0800

    goodchild增加exprice 换款价格字段

--- src/main/java/com/jinlele/model/GoodChild.java
+++ src/main/java/com/jinlele/model/GoodChild.java
@@ -5,11 +5,27 @@ import java.util.Date;
 public class GoodChild {
     private Integer id;
 
+    private Integer goodId;
+
     private String color;
 
-    private Integer stocknumber;
+    private String weight;
 
-    private Integer goodId;
+    private String material;
+
+    private String mainstorespec;
+
+    private String handsize;
+
+    private String necklaceLen;
+
+    private String braceletLen;
+
+    private Double exprice;
+
+    private Double price;
+
+    private Integer stocknumber;
 
     private String imgurl;
 
@@ -27,6 +43,14 @@ public class GoodChild {
         this.id = id;
     }
 
+    public Integer getGoodId() {
+        return goodId;
+    }
+
+    public void setGoodId(Integer goodId) {
+        this.goodId = goodId;
+    }
+
     public String getColor() {
         return color;
     }
@@ -35,20 +59,76 @@ public class GoodChild {
         this.color = color == null ? null : color.trim();
     }
 
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight == null ? null : weight.trim();
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material == null ? null : material.trim();
+    }
+
+    public String getMainstorespec() {
+        return mainstorespec;
+    }
+
+    public void setMainstorespec(String mainstorespec) {
+        this.mainstorespec = mainstorespec == null ? null : mainstorespec.trim();
+    }
+
+    public String getHandsize() {
+        return handsize;
+    }
+
+    public void setHandsize(String handsize) {
+        this.handsize = handsize == null ? null : handsize.trim();
+    }
+
+    public String getNecklaceLen() {
+        return necklaceLen;
+    }
+
+    public void setNecklaceLen(String necklaceLen) {
+        this.necklaceLen = necklaceLen == null ? null : necklaceLen.trim();
+    }
+
+    public String getBraceletLen() {
+        return braceletLen;
+    }
+
+    public void setBraceletLen(String braceletLen) {
+        this.braceletLen = braceletLen == null ? null : braceletLen.trim();
+    }
+
+    public Double getExprice() {
+        return exprice;
+    }
+
+    public void setExprice(Double exprice) {
+        this.exprice = exprice;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
     public Integer getStocknumber() {
         return stocknumber;
     }
 
     public void setStocknumber(Integer stocknumber) {
         this.stocknumber = stocknumber;
-    }
-
-    public Integer getGoodId() {
-        return goodId;
-    }
-
-    public void setGoodId(Integer goodId) {
-        this.goodId = goodId;
     }
 
     public String getImgurl() {
